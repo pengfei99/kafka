abdfbec656d5cc522fba2b86e34f0268ceebaa4c
Pengfei Liu <liu.pengfei@hotmail.fr>
2022-03-03 14:49:53 +0100

    refactor code

--- projects/KafkaProducer/src/main/java/org/pengfei/CustomerProducer.java
+++ projects/KafkaProducer/src/main/java/org/pengfei/CustomerProducer.java
@@ -12,17 +12,20 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
 public class CustomerProducer {
+    private final String brokerUrl;
+    private final String topicName;
+    private final KafkaProducer producer;
+    private final Long messageCount;
 
-    public static void main(String[] args) {
-        int MESSAGE_COUNT = 1000;
-        String topicName = "test-topic";
-        String BROKERS_URL= "pengfei.org:9092";
-
+    public CustomerProducer(String brokerUrl, String topicName, Long messageCount) {
+        this.brokerUrl = brokerUrl;
+        this.topicName = topicName;
+        this.messageCount=messageCount;
         /** Step1 : set up the producer config*/
         Properties props = new Properties();
         // These three config is mandatory, we can't omit them
         // ProducerConfig.BOOTSTRAP_SERVERS_CONFIG == "bootstrap.servers"
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BROKERS_URL);
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, brokerUrl);
         // The keySerializer will be used to serialize the key encapsulated in ProducerRecord.
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, LongSerializer.class.getName());
         // The ValueSerializer will be used to serialize the value encapsulated in ProducerRecord.
@@ -48,22 +51,24 @@ public class CustomerProducer {
         //3 secs
         props.put("max.block.ms", 3000);
 
-
         /** Step2: build the kafka producer instance*/
-        KafkaProducer p1 = new KafkaProducer<>(props);
+        this.producer = new KafkaProducer<>(props);
 
-        /** Step3: Send the message and get reply*/
+    }
+
+    public void sendWithSyncMode(){
         // key is optional, can be omitted
         Long key = 10000000L;
-        for (int i = 0; i < MESSAGE_COUNT; i++) {
+        for (int i = 0; i < this.messageCount; i++) {
             ProducerRecord<Long, String> record =
-                    new ProducerRecord<>(topicName, key + i, "test-value" + i);
+                    new ProducerRecord<>(topicName, key + i, "test-value: " + i);
 
             // get metadata after a synchronously send
             try {
-                Future response = p1.send(record);
+                Future response = this.producer.send(record);
+                // in the metadata, you can get the topic, partition and offset of the message
                 RecordMetadata metadata = (RecordMetadata) response.get();
-                System.out.println("Record sent with key " + i + " to partition " + metadata.partition()
+                System.out.println("Record sent with key " + i + " to topic " + metadata.topic()+" to partition " + metadata.partition()
                         + " with offset " + metadata.offset());
             } catch (ExecutionException e) {
                 System.out.println("Error in sending record");
@@ -73,10 +78,45 @@ public class CustomerProducer {
                 System.out.println(e);
             }
         }
+    }
+
+    public String getBrokerUrl() {
+        return brokerUrl;
+    }
+
+    public String getTopicName() {
+        return topicName;
+    }
+
+    public KafkaProducer getProducer() {
+        return producer;
+    }
+
+    public void close(){
+        this.producer.close();
+    }
+
+    public static void main(String[] args) {
+        long MESSAGE_COUNT = 1000L;
+        String topicName = "test-topic";
+        String BROKERS_URL = "pengfei.org:9092";
+        CustomerProducer cProducer = new CustomerProducer(BROKERS_URL, topicName,MESSAGE_COUNT);
+
+
+        /** Step3: Send the message and get reply
+         *
+         * The send() method  has 3 different mode:
+         * - Async mode without callback : send message without waiting any response.
+         * - Async mode with callback: send message, if success a call back function will be called
+         * - Sync mode: send message wait a response.
+         * */
+
+        /** Sync mode: */
+        cProducer.sendWithSyncMode();
 
 
         /** Step4: close the producer connexion.*/
-        p1.close();
+
     }
 }
 
