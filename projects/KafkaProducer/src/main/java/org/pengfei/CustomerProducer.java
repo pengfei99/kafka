498ee99f0ea1987bd74342d906053d01437b396a
Pengfei Liu <liu.pengfei@hotmail.fr>
2022-03-04 11:47:10 +0100

    add custom partitioner

--- projects/KafkaProducer/src/main/java/org/pengfei/CustomerProducer.java
+++ projects/KafkaProducer/src/main/java/org/pengfei/CustomerProducer.java
@@ -13,46 +13,68 @@ import java.util.concurrent.Future;
 public class CustomerProducer {
     private final String brokerUrl;
     private final String topicName;
-    private final KafkaProducer producer;
+    private KafkaProducer producer;
     private final Long messageCount;
+    private Properties config;
 
     public CustomerProducer(String brokerUrl, String topicName, Long messageCount) {
         this.brokerUrl = brokerUrl;
         this.topicName = topicName;
         this.messageCount = messageCount;
         /** Step1 : set up the producer config*/
-        Properties props = new Properties();
+        setConfig();
+        /** Step2: build the kafka producer instance*/
+        init();
+
+    }
+
+    public CustomerProducer(String brokerUrl, String topicName, Long messageCount, String customPartitioner) {
+        this.brokerUrl = brokerUrl;
+        this.topicName = topicName;
+        this.messageCount = messageCount;
+        /** Step1 : set up the producer config*/
+        setConfig();
+        // add customPartitioner to producer config
+        config.put(ProducerConfig.PARTITIONER_CLASS_CONFIG,customPartitioner);
+
+        /** Step2: build the kafka producer instance*/
+        init();
+    }
+
+    public void init(){
+        /** Step2: build the kafka producer instance*/
+        this.producer = new KafkaProducer<>(config);
+    }
+
+    public void setConfig(){
+        this.config = new Properties();
         // These three config is mandatory, we can't omit them
         // ProducerConfig.BOOTSTRAP_SERVERS_CONFIG == "bootstrap.servers"
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, brokerUrl);
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, brokerUrl);
         // The keySerializer will be used to serialize the key encapsulated in ProducerRecord.
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, LongSerializer.class.getName());
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, LongSerializer.class.getName());
         // The ValueSerializer will be used to serialize the value encapsulated in ProducerRecord.
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
         // It's recommended to assign an ID to each client(producer and consumer)
-        props.put(ProducerConfig.CLIENT_ID_CONFIG, "exp1_p1");
+        config.put(ProducerConfig.CLIENT_ID_CONFIG, "exp1_p1");
 
         // Properties which modify the behaviours of the producer. For example make the message safer, or has higher
         // throughput
 
         // The two lines are equals.
-        //props.put(ProducerConfig.ACKS_CONFIG,"-1");
-        props.put("acks", "1");
+        //config.put(ProducerConfig.ACKS_CONFIG,"-1");
+        config.put("acks", "1");
 
-        props.put("retries", 3);
+        config.put("retries", 3);
         //batch size is 3 MB
-        props.put("batch.size", 323840);
+        config.put("batch.size", 323840);
         // the sender will wait 0.01 sec for filling the batch before sending it.
-        props.put("linger.ms", 10);
+        config.put("linger.ms", 10);
         //buffer size is 32MB
-        props.put("buffer.memory", 33554432);
+        config.put("buffer.memory", 33554432);
         //3 secs
-        props.put("max.block.ms", 3000);
-
-        /** Step2: build the kafka producer instance*/
-        this.producer = new KafkaProducer<>(props);
-
+        config.put("max.block.ms", 3000);
     }
 
     /**
@@ -143,33 +165,5 @@ public class CustomerProducer {
         this.producer.close();
     }
 
-    public static void main(String[] args) {
-        long MESSAGE_COUNT = 1000L;
-        String topicName = "test-topic";
-        String BROKERS_URL = "pengfei.org:9092";
-        CustomerProducer cProducer = new CustomerProducer(BROKERS_URL, topicName, MESSAGE_COUNT);
-
-
-        /** Step3: Send the message and get reply
-         *
-         * The send() method  has 3 different mode:
-         * - Async mode without callback : send message without waiting any response.
-         * - Async mode with callback: send message, if success a call back function will be called
-         * - Sync mode: send message wait a response.
-         * */
-
-        /** Async mode: */
-        //  cProducer.sendAsyncMode();
-
-        /** Async mode with call back:  */
-       cProducer.sendAsyncModeCallback();
-
-        /** Sync mode: */
-        //cProducer.sendWithSyncMode();
-
-
-        /** Step4: close the producer connexion.*/
-        cProducer.close();
-    }
 }
 
